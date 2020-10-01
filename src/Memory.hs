13757d060579e38982eab8b57fff53b3c28b0b77
Inoki <veyx.shaw@gmail.com>
2020-10-01 20:24:10 +0200

    Fix command, not yet GBA

--- src/Memory.hs
+++ src/Memory.hs
@@ -3,5 +3,5 @@ module Memory where
 import Data.Word
 import Data.Bits
 
--- GBA has a ARM7 32bit processor and 32bit address space
+-- An ARM7 32bit processor and 32bit address space
 type Address = Word32
