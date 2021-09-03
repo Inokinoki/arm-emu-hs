57904104281107d743043cd42b5a2701f7c37f27
Inoki <veyx.shaw@gmail.com>
2021-09-03 21:21:54 +0200

    Remove unnecessary Format header

--- src/InstructionSet/ARM/Instruction.hs
+++ src/InstructionSet/ARM/Instruction.hs
@@ -12,9 +12,7 @@ import Instruction
 -- ARM ISA format:
 --                          Format  |31 |30 |29 |28 |27 |26 |25 |24 |23 |22 |21 |20 |19 |18 |17 |16 |15 |14 |13 |12 |11 |10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
 --  Data Processing Immediate shift:|    cond[1]    | 0 | 0 | 0 |     opcode    | S |       Rn      |       Rd      |    shift amount   | shift | 0 |       Rm      |
---                          Format  |31 |30 |29 |28 |27 |26 |25 |24 |23 |22 |21 |20 |19 |18 |17 |16 |15 |14 |13 |12 |11 |10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
 --  Data Processing register shift: |    cond[1]    | 0 | 0 | 0 |     opcode    | S |       Rn      |       Rd      |       Rs      | 0 | shift | 1 |       Rm      |
---                          Format  |31 |30 |29 |28 |27 |26 |25 |24 |23 |22 |21 |20 |19 |18 |17 |16 |15 |14 |13 |12 |11 |10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
 --  Data Processing Immediat:       |    cond[1]    | 0 | 0 | 1 |     opcode    | S |       Rn      |       Rd      |     Rotate    |           Immediate           |
 decodeDataProcessing :: Word32 -> Maybe Instruction
 decodeDataProcessing x
@@ -24,7 +22,6 @@ decodeDataProcessing x
 
 --                          Format  |31 |30 |29 |28 |27 |26 |25 |24 |23 |22 |21 |20 |19 |18 |17 |16 |15 |14 |13 |12 |11 |10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
 --  Load/Store Immediate Offset:    |    cond[1]    | 0 | 1 | 0 | P | U | B | W | L |       Rn      |       Rd      |                  Immediate                    |
---                          Format  |31 |30 |29 |28 |27 |26 |25 |24 |23 |22 |21 |20 |19 |18 |17 |16 |15 |14 |13 |12 |11 |10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
 --  Load/Store Register Offset:     |    cond[1]    | 0 | 1 | 1 |     opcode    | S |       Rn      |       Rd      |    shift amount   | shift | 0 |       Rm      |
 decodeLoadStoreWithOffset :: Word32 -> Maybe Instruction
 decodeLoadStoreWithOffset x
